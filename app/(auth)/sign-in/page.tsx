bfedd49ff43b333bc54e1496786e9d0a8a5b976e
Gokul <gokulchandan24@gmail.com>
2024-06-16 00:06:14 +0530

    app initialization

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+const SignIn = () => {
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      {/* <AuthForm type="sign-in" /> */}Signin
+    </section>
+  );
+};
+
+export default SignIn;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+const SignUp = async () => {
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      {/* <AuthForm type="sign-up" /> */}Signup
+    </section>
+  );
+};
+
+export default SignUp;
